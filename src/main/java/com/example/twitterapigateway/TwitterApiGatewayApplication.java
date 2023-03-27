acb17de087e9afbefc78b9f5f4fa6b1881dd1e9c
Sem de Wilde <sem.dewilde6@gmail.com>
2023-03-28 01:24:12 +0200

    Initial commit

---
+++ src/main/java/com/example/twitterapigateway/TwitterApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApiGatewayApplication.class, args);
+    }
+
+}
